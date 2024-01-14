0826ee520d4229f4e1e3a6896790851746e50c6e
Gills <38540024+CyberoidG@users.noreply.github.com>
2024-01-14 15:21:12 -0700

    Update package-info.java

--- package-info.java
+++ package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "Thaumcraft", apiVersion = "6.0.2", provides = "Thaumcraft|API")
+@API(owner = "Thaumcraft", apiVersion = "1.0.1", provides = "Thaumcraft|API")
 package thaumcraft.api;
 
 import net.minecraftforge.fml.common.API;
